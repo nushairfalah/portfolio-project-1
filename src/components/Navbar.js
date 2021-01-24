b647d3784eba5fccee618d550d9a20f7126b10e7
falah <nushairfalahizz@gmail.com>
2021-01-24 12:39:32 +0700

    Header Complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../m-logo.png';
+import logo from '../just-do-it.png';
 // REACT FONTAWESOME IMPORTs
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
